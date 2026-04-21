832f30c3e189ca8bd01a415b3b6b476c0732c46d
Lucas Cuppi <lucascuppi@gmail.com>
2026-04-21 09:30:55 -0300

    adicionando animacoes com js

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ function abrirMenu() {
 }
 
 btnMobile.addEventListener("click", abrirMenu);
+
+const sections = document.querySelectorAll(".js-animacao");
+const userWindow = window.innerHeight * 0.4;
+
+function sectionAnimar() {
+  sections.forEach((section) => {
+    const sectionTop = section.getBoundingClientRect().top;
+    const sectionVisivel = sectionTop - userWindow < 0;
+    if (sectionVisivel) {
+      section.classList.add("ativo");
+    } else {
+      section.classList.remove("ativo");
+    }
+  });
+}
+
+window.addEventListener("scroll", sectionAnimar);
